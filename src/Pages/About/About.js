83100ec49c1cdde19548244586a2431dc548eb10
nurgazievbekbolsun <nurgazievbekbolsun906@gmail.com>
2023-05-21 15:22:00 +0600

    addd

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,6 +8,7 @@ const About = () => {
                     <div className="about-block">
                         <h1>132456789</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, recusandae?</p>
+                        <p>Lorem ipsum dolor sit amet, recusandae?</p>
                     </div>
                 </div>
             </div>

ee44a323ff4c87acaca0e3f97e950c1f7a8f5928
abytaeva11 <113246152+abytaeva11@users.noreply.github.com>
2023-05-21 14:33:04 +0500

    navi

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -10,6 +10,10 @@ const About = () => {
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, recusandae?</p>
                         <p>Lorem ipsum dolor sit amet, recusandae?</p>
                         <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, odio?</h2>
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, odio?</h2>
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, odio?</h2>
+
+
                     </div>
                 </div>
             </div>

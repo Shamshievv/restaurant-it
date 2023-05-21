2d16be536be1451076706b2eae4ed5fe17437534
nurgazievbekbolsun <nurgazievbekbolsun906@gmail.com>
2023-05-21 16:23:37 +0600

    bak

--- src/Pages/Visit/Visit.js
+++ src/Pages/Visit/Visit.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Visit = () => {
     return (
-        <div>
+        <div id="visit">
+            <div className="container">
+                <div className="visit">
+                    <div className="visit--block">
+                        <h1>Lorem ipsum dolor sit.</h1>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );

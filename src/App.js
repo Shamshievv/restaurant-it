52d604e49650abb0b6c7651df7d6e9d497e22832
Beku001 <126578152+nurgazievbekbolsun@users.noreply.github.com>
2023-05-24 19:21:01 +0600

    Text (#33)
    * beku
    
    * textt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss';
 import Header from "./components/Header/Header";
 import Hero from "./Pages/Hero/Hero";
 import Visit from "./Pages/Visit/Visit";
-import Footer from "./Pages/Footer/Footer";
 import Best from "./Pages/BestSellers/Best";
+import Footer from "./Pages/Footer/Footer";
 import Main from "./Pages/MainMenu/Routes/Routes";
-import {Route, Routes} from "react-router-dom";
 import About from "./Pages/About/About";
-
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Hero/>
-      <Routes>
-          <Route path={"/about"} element={<About/>}/>
-      </Routes>
-        <About/>
-      <Best/>
-      <Main/>
-      <Visit/>
-      <Best/>
+        <Hero/>
+        <Main/>
+      <About/>
+        <Best/>
+        <Visit/>
       <Footer/>
     </div>
   );
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import './../.././Styles/About.scss'
+import coking from './../../images/Frame 15.png'
+import coki from './../../images/Frame 16.png'
+import logo from './../../images/about-logo.svg'
 
 const About = () => {
     return (
         <div id="about">
             <div className="container">
                 <div className="about">
-                    <div className="about-block">
-                       About
+                    <div className="about--block">
+                     <div className="about--block__group">
+                         <img src={logo} alt="img"/>
+                         <h1>A Journey Throught
+                             Cafesio Flavors</h1>
+                     </div>
+                        <p>Try dishes that will open up new tastes for you and delight your
+                            eyes with their appearance. Here you will find a cozy atmosphere,
+                            excellent service and attention to each guest. Book a table now
+                            and enjoy a unique experience of taste discovery!</p>
+                    </div>
+                    <div className="about--images">
+                        <img src={coking} alt="img"/>
+                        <img src={coki} alt="img"/>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };

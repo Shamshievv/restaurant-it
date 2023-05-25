a233d8abed2a35ad017fd1d5007373815d7a19fb
Azamat <azamatshamshiev18@gmail.com>
2023-05-25 15:17:53 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-
 import './App.scss';
 import Header from "./components/Header/Header";
 import Hero from "./Pages/Hero/Hero";
 import Visit from "./Pages/Visit/Visit";
 import Best from "./Pages/BestSellers/Best";
 import Footer from "./Pages/Footer/Footer";
-import Main from "./Pages/MainMenu/Routes/Routes";
-import About from "./Pages/About/About";
+
+import {Route, Routes} from "react-router-dom";
+import MainN from "./Pages/Main/MainN";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-        <Hero/>
-        <Main/>
-      <About/>
-        <Best/>
-        <Visit/>
+         <Routes>
+             <Route path={"/"} element={<MainN/>}/>
+         </Routes>
       <Footer/>
     </div>
   );
---
+++ src/Pages/Main/MainN.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Hero from "../Hero/Hero";
+import About from "../About/About";
+import Best from "../BestSellers/Best";
+import Main from "../MainMenu/Routes/Routes";
+import ModernInterier from "../ModernInterier/ModernInterier";
+import Visit from "../Visit/Visit";
+
+const MainN = () => {
+    return (
+        <div>
+           <Hero/>
+           <About/>
+            <Best/>
+            <Main/>
+            <ModernInterier/>
+            <Visit/>
+        </div>
+    );
+};
+
+export default MainN;

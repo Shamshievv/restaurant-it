13e471ce055bae106a6d9298cc3f25f3a4e11f74
abytaeva11 <113246152+abytaeva11@users.noreply.github.com>
2023-05-27 17:08:28 +0500

    header and modern (#50)

--- src/Pages/ModernInterier/ModernInterier.js
+++ src/Pages/ModernInterier/ModernInterier.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import bg from "../../../src/images/restuarant1.png"
+import bg2 from "../../images/restaurant2.png"
+import bg3 from "../../images/restaurant3.png"
+import bg4 from "../../images/restaurant4.png"
+import bg5 from "../../images/restaurant5.png"
+import vector from "../../images/hero-wave.svg"
+import vect from   "../../images/hero-wave-right.svg"
+const Modern = () => {
 
-const ModernInterier = () => {
-    return (
-        <div>
-          modern
-        </div>
+           return (
+               <div id="modern">
+           <div className="container">
+           <div className="line"/>
+           <div className="modern--text">
+           <div className="modern--text__block">
+           <img src={vector} alt="" className="modern--text__block--img"/>
+           </div>
+           <h1 className="modern--text__title">Modern Interior</h1>
+           <div className="modern--text__block">
+           <img src={vect} alt="" className="modern--text__block--img"/>
+           </div>
+           </div>
+           <div className="modern">
+           <div className="modern--img">
+           <img src={bg} alt=""/>
+           </div>
+           <div className="modern--image">
+           <img src={bg2} alt=""/>
+           <div className="modern--image__fon">
+           <img src={bg3} alt=""/>
+           <img src={bg4} alt=""/>
+           </div>
+           </div>
+           <div className="modern--img">
+           <img src={bg5} alt=""/>
+           </div>
+           <div className="modern--img">
+           <img src={bg} alt=""/>
+           </div>
+           <div className="modern--image">
+           <img src={bg2} alt=""/>
+           <div className="modern--image__fon">
+           <img src={bg3} alt=""/>
+           <img src={bg4} alt=""/>
+           </div>
+           </div>
+           <div className="modern--img">
+           <img src={bg5} alt=""/>
+           </div>
+           </div>
+           </div>
+           </div>
     );
 };
 
-export default ModernInterier;
+export default Modern;

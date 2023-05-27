710bad9f0aef59f83ad8aadf1f7197b470cf5ffc
abytaeva11 <113246152+abytaeva11@users.noreply.github.com>
2023-05-27 14:33:26 +0500

    Again menu (#49)
    * wizard
    
    * menu
    
    * about
    
    * you
    
    * again add menu
    
    * again menu

---
+++ src/Pages/Menu/MenuAboute/AboutGlobal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutFoods from "./AboutFoods/MenuAboute";
+
+const Menu = () => {
+    return (
+        <div>
+            <AboutFoods/>
+        </div>
+    );
+};
+
+export default Menu;
---
+++ src/data/Data.js
@@ -0,0 +1,295 @@
+
+import DessertsMenu1 from "../images/DessertsMenu1.png";
+import DessertsMenu2 from "../images/DessertsMenu2.png";
+import DessertsMenu3 from "../images/DessertsMenu3.png";
+import DessertsMenu4 from "../images/DessertsMenu4.png";
+import DessertsMenu5 from "../images/DessertsMenu5.png";
+import DessertsMenu6 from "../images/DessertsMenu6.png";
+
+import HotDrinks from "../images/HotDrinks.jpg";
+import ColdDrinksImg from "../images/ColdDrinks.png";
+import Eastern from "../images/EasternCuisineMenu.jpeg";
+import FastFood from "../images/FastFood.jpg"
+import National from "../images/NationalFood.jpg"
+
+export const Foods = [
+    {
+        id: 1,
+        title: "Desserts",
+        isDone: false,
+        nameFoots: [
+            {
+                id:1,
+                images:DessertsMenu1,
+                name:"Ice Cream",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:2,
+                images:DessertsMenu2,
+                name:"Ice Cream",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:3,
+                images:DessertsMenu3,
+                name:"Ice Cream",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:4,
+                images:DessertsMenu4,
+                name:"Ice Cream",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:5,
+                images:DessertsMenu5,
+                name:"Ice Cream",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:6,
+                images:DessertsMenu6,
+                name:"Ice Cream",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+        ]
+    },
+    {
+        id:2,
+        title: "Hot Drinks",
+        isDone: false,
+        nameFoots: [
+            {
+                id:1,
+                images:HotDrinks,
+                name:"ASSAM",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:2,
+                images:HotDrinks,
+                name:"ASSAM",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            }, {
+                id:3,
+                images:HotDrinks,
+                name:"ASSAM",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            }, {
+                id:4,
+                images:HotDrinks,
+                name:"ASSAM",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            }, {
+                id:5,
+                images:HotDrinks,
+                name:"ASSAM",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:6,
+                images:HotDrinks,
+                name:"ASSAM",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Cold Drinks",
+        isDone: false,
+        nameFoots: [
+            {
+                id:1,
+                images:ColdDrinksImg,
+                name:" Martin",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:2,
+                images:ColdDrinksImg,
+                name:"Martin",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            }, {
+                id:3,
+                images:ColdDrinksImg,
+                name:"Martin",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            }, {
+                id:4,
+                images:ColdDrinksImg,
+                name:"Martin",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            }, {
+                id:5,
+                images:ColdDrinksImg,
+                name:"Martin",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+            {
+                id:6,
+                images:ColdDrinksImg,
+                name:"Martin",
+                price:"9.11",
+                desert:"soda,cream,milk,sugar"
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: "National Foods",
+        isDone: false,
+        nameFoots: [
+            {
+                id:4,
+                name: "Baiel Brewery",
+                images: National,
+                price: "$24",
+                text: "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:4,
+                name: "Beer Brewery",
+                images: National,
+                price: "$24",
+                text: "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:4,
+                name: "Beer Brewery",
+                images: National,
+                price: "$24",
+                text: "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:4,
+                name: "Beer Brewery",
+                images: National,
+                price: "$24",
+                text: "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:4,
+                name: "Beer Brewery",
+                images: National,
+                price: "$24",
+                text: "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:4,
+                name: "Beer Brewery",
+                images: National,
+                price: "$24",
+                text: "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+                desert: "soda,cream,milk,sugar",
+            },
+        ]
+    },
+    {
+        id: 5,
+        title: "Eastern cuisine",
+        isDone: false,
+        nameFoots: [
+            {
+                id: 5,
+                name: "Ali Brewery",
+                images: Eastern,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id: 5,
+                name: "Beer Brewery",
+                images: Eastern,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id: 5,
+                name: "Beer Brewery",
+                images: Eastern,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id: 5,
+                name: "Beer Brewery",
+                images: Eastern,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id: 5,
+                name: "Beer Brewery",
+                images: Eastern,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id: 5,
+                name: "Beer Brewery",
+                images: Eastern,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            },
+        ]
+    },
+    {
+        id: 6,
+        title: "Fast foods",
+        isDone: false,
+        nameFoots: [
+            {
+                id:6,
+                name: "Beer Brewery",
+                images: FastFood,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:6,
+                name: "Beer Brewery",
+                images: FastFood,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:6,
+                name: "Beer Brewery",
+                images: FastFood,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:6,
+                name: "Beer Brewery",
+                images: FastFood,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:6,
+                name: "Beer Brewery",
+                images: FastFood,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            }, {
+                id:6,
+                name: "Beer Brewery",
+                images: FastFood,
+                price: "$24",
+                desert: "soda,cream,milk,sugar",
+            },
+        ]
+    },
+]
